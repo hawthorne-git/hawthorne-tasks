6267ad8e2ed7b7ba62e44e4d8c19e1286e696c0b
HSCONickMachin <hsco.ben@gmail.com>
2022-01-06 15:59:45 -0500

    image reduction

---
+++ ImageCompressor/testing_compression.rb
@@ -0,0 +1,16 @@
+require "vips"
+require 'open-uri'
+
+
+image_path = "C:/Users/hawth/Desktop/Luna Fawn.png"
+temp_png = "temp/1_image_png.png"
+temp_jpg = "temp/2_image_jpeg.jpg"
+temp_webp = "temp/final2reduce4.webp"
+
+a = Vips::Image.new_from_file image_path
+a.pngsave(temp_png, :compression => 1, :palette => true, :strip => true)
+a.jpegsave(temp_jpg, :optimize_coding => true)
+b = Vips::Image.new_from_file temp_jpg
+b.webpsave(temp_webp, :min_size => true)
+
+#resize later find out which reduction works better png or jpg
