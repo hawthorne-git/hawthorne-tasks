f42f71c4c1eae6da3d35147d425f22a544049e05
Charlie Prezzano <charlie@hawthornethreads.com>
2024-02-15 10:30:28 -0500

    unknown change

--- Etsy3/run.rb
+++ Etsy3/run.rb
@@ -3,9 +3,9 @@ require 'rest-client'
 
 # ------------------------------------------------------------------------------------
 
-api_keystring = 'htz.....'
+api_keystring = 'api_keystring'
 
-access_token = '626126701...'
+access_token = 'access_token'
 
 # ------------------------------------------------------------------------------------
 
@@ -20,53 +20,8 @@ request_options_with_authorization = {
 
 # ------------------------------------------------------------------------------------
 
-uri = URI('https://openapi.etsy.com/v3/application/listings/1206223359')
-req = Net::HTTP::Get.new(uri)
-req['x-api-key'] = api_keystring
-
-res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http|
-  http.request(req)
-}
-
-#puts res.body.to_s
-
-# ------------------------------------------------------------------------------------
-
-uri = URI('https://openapi.etsy.com/v3/application/shops/35063660/transactions')
-req = Net::HTTP::Get.new(uri)
-req['x-api-key'] = api_keystring
-req['authorization'] = 'Bearer ' + access_token
-
-res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') { |http|
-  http.request(req)
-}
-
-#puts res.body.to_s
-
-# ------------------------------------------------------------------------------------
-
 begin
-  #response = RestClient.get 'https://openapi.etsy.com/v3/application/listings/1301252552', headers = request_options_with_authorization
-  #puts response.body.to_s
-rescue RestClient::Unauthorized => e
-  puts e.message.to_s
-end
-
-# ------------------------------------------------------------------------------------
-
-begin
-  response = RestClient.get 'https://openapi.etsy.com/v3/application/shops/35063660/sections', headers = request_options_with_authorization
-  #puts response.body.to_s
-rescue RestClient::NotFound => e
-  puts e.message.to_s
-end
-
-# ------------------------------------------------------------------------------------
-
-puts ''
-
-begin
-  response = RestClient.get 'https://openapi.etsy.com/v3/application/shops/35063660/listings/1301271444/properties', headers = request_options_with_authorization
+  response = RestClient.get 'https://openapi.etsy.com/v3/application/listings/1315837451', headers = request_options_without_authorization
   puts response.body.to_s
 rescue RestClient::ExceptionWithResponse => e
   puts 'EXCEPTION CAUGHT'
