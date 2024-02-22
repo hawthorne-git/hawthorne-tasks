705611742e2900ae144277e7b06888e0b60b66ab
Charlie Prezzano <charlie@hawthornethreads.com>
2024-02-22 16:31:42 -0500

    aws rekongition

--- AWSImageRekognition/analyze_image.rb
+++ AWSImageRekognition/analyze_image.rb
@@ -1,26 +1,59 @@
 require 'aws-sdk-rekognition'
+require 'json/ext'
 
-# ---------------------------------------------- Establish a connection to AWS s2
-# https://docs.aws.amazon.com/rekognition/latest/dg/labels-detect-labels-image.html
-# https://docs.aws.amazon.com/rekognition/latest/dg/procedure-moderate-images.html
+# ----------------------------------------------
+
+AWS_IMAGE_KEY = 'swimwear_test.png'
+
+AWS_IMAGE_KEY = '12e871x5x3wbeh7ae8muvtfkqkna'
+
+# ----------------------------------------------
 
 client = Aws::Rekognition::Client.new(
   region: 'us-east-2',
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 )
 
+# ----------------------------------------------
+
 attrs = {
   image: {
     s3_object: {
       bucket: 'idesign-s3-bucket',
-      name: '086klwrecq1e3urtphkmjetvox0w'
+      name: AWS_IMAGE_KEY
     },
   },
-  max_labels: 10
+  max_labels: 20
 }
 
 response = client.detect_labels attrs
+puts response.to_s
 
+response[:labels].each do |label|
+  puts ''
+  puts label.to_s
+  puts label[:name]
+end
+
+return if true
+
+# ----------------------------------------------
+
+puts ''
+puts ''
+
+# ----------------------------------------------
+
+attrs = {
+  image: {
+    s3_object: {
+      bucket: 'idesign-s3-bucket',
+      name: AWS_IMAGE_KEY
+    },
+  }
+}
+
+response = client.detect_moderation_labels attrs
 puts response.to_s
 
 # ----------------------------------------------
