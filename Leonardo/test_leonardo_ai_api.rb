55d3e2095a69e3e779f453d56e2016a1e6ce64f2
Scott Palocsik <hsco.scott.palocsik@gmail.com>
2024-07-09 09:35:16 -0400

    scott commit - create_image working, returns images generation details using get request with generationID

--- Leonardo/test_leonardo_ai_api.rb
+++ Leonardo/test_leonardo_ai_api.rb
@@ -7,7 +7,9 @@ puts 'Starting ...'
 
 # ---------------------------------------------------------------------------
 
-get_user_info = true
+get_user_info = false
+
+create_image = false
 
 # ---------------------------------------------------------------------------
 
@@ -15,6 +17,26 @@ header = {
   Authorization: 'Bearer ' + ENV['LEONARDO_TOKEN'],
   content_type: :json
 }
+
+# Fetch generationID method for getting images ------------------------------
+
+def fetch_generation_details(generation_id)
+  url = "https://cloud.leonardo.ai/api/rest/v1/generations/#{generation_id}"
+
+  headers = {
+    accept: 'application/json',
+    Authorization: "Bearer #{ENV['LEONARDO_TOKEN']}"
+  }
+
+  begin
+
+    response = RestClient.get(url, headers)
+
+    puts "Response code: #{response.code}"
+    puts "Response body: #{response.body}"
+
+  end
+end
 
 # ---------------------------------------------------------------------------
 
@@ -27,3 +49,42 @@ if get_user_info
   puts response.to_s
 
 end
+
+# ---------------------------------------------------------------------------
+
+if create_image
+  url = 'https://cloud.leonardo.ai/api/rest/v1/generations'
+
+  values = {
+    "height": 512,
+    "width": 512,
+    "modelId": "b24e16ff-06e3-43eb-8d33-4416c2d75876",
+    "prompt": "coffee in a diner",
+    "num_images": 1
+  }
+
+  begin
+    response = RestClient.post(
+      url,
+      values.to_json,
+      header
+    )
+
+    # Parse JSON response
+    parsed_response = JSON.parse(response.body)
+
+    # Extract generation ID and API Credit Cost from sdGenerationJob
+    generation_id = parsed_response['sdGenerationJob']['generationId']
+    api_credit_cost = parsed_response['sdGenerationJob']['apiCreditCost']
+
+    # Now use this generation ID to fetch details of this specific generation
+    fetch_generation_details(generation_id)
+
+    puts "Generation ID: #{generation_id}"
+    puts "API Credit Cost: #{api_credit_cost}"
+    puts "Response body:#{response.body}"
+
+  end
+
+end
+
